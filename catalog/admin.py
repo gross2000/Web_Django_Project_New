9790422039761a3e986437bad4c9cc0cabbaba6b
cheandrey <skyandreymann@gmail.com>
2024-09-11 23:37:19 +0300

    Фильтрация

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from catalog.models import Product, Category
 
-# Register your models here.
+# Админка для модели Category
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # Указываем, какие поля отображать в списке
+    search_fields = ('name',)       # Добавляем возможность поиска по имени категории
+
+# Админка для модели Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')  # Отображаем необходимые поля
+    list_filter = ('category',)                          # Добавляем фильтрацию по категории
+    search_fields = ('name', 'description')              # Добавляем возможность поиска по имени и описанию продукта
+
